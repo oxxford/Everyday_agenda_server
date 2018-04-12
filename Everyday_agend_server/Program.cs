6c787df5205c2327b84e70cf96d28048058deada
Dmitry Grigorev <gridmi007@gmail.com>
2018-04-12 19:25:36 +0300

    Starting point

---
+++ Everyday_agend_server/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Nancy;
+using Nancy.Hosting.Self;
+
+namespace Everyday_agend_server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var host = new NancyHost(new Uri("http://localhost:8080")))
+            {
+                host.Start();
+                Console.WriteLine("Running on http://localhost:8080");
+                Console.ReadLine();
+            }
+        }
+    }
+
+    public class HelloModule : NancyModule
+    {
+        public HelloModule()
+        {
+            Get["/{name}/{id}"] = parameters =>
+            {
+                Console.Write((String)parameters.id);
+                return parameters.name;
+            };
+        }
+    }
+}

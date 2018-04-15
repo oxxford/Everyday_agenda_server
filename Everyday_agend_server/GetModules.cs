6059ec70094a2687e841662bf67b938ff8a48eca
Dmitry Grigorev <gridmi007@gmail.com>
2018-04-15 16:05:48 +0300

    Added impression module

--- Everyday_agend_server/GetModules.cs
+++ Everyday_agend_server/GetModules.cs
@@ -1,12 +1,41 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
+using Nancy;
 
 namespace Everyday_agend_server
 {
-    class GetModules
+    class GetModules : NancyModule
     {
+        public GetModules()
+        {
+            Get["/getimage/userid={userid}&imageid={imageid}"] = parameters =>
+            {
+                //TODO verification
+
+                return createFileResponse("image/png", parameters.userid, parameters.imageid);
+            };
+
+            Get["/getvideo/userid={userid}&videoid={videoid}"] = parameters =>
+            {
+                //TODO verification
+
+                return createFileResponse("video/mp4", parameters.userid, parameters.videoid);
+            };
+        }
+
+        private Response createFileResponse(String contentType, String userId, String itemId)
+        {
+            return new Response
+            {
+                ContentType = contentType,
+
+                Contents = s =>
+                {
+                    String fileName = "\\" + userId + "\\" + itemId;
+                    using (var stream = new FileStream(fileName, FileMode.Open))
+                        stream.CopyTo(s);
+                }
+            };
+        }
     }
 }

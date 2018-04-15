e5ca9f014885e3be8fd05dadb2039dd7fd6d3774
Dmitry Grigorev <gridmi007@gmail.com>
2018-04-15 10:30:43 +0300

    Database adapter added

---
+++ Everyday_agend_server/DatabaseAdapter.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+
+namespace Everyday_agend_server
+{
+    class DatabaseAdapter
+    {
+        public int getImageId(DateTime date, int userId)
+        {
+            //TODO
+            return 0;
+        }
+
+        public int getVideoId(DateTime date, int userId)
+        {
+            //TODO
+            return 0;
+        }
+
+        public String getText(DateTime date, int userId)
+        {
+            //TODO
+            return null;
+        }
+
+        public int getUserId(String username, String password)
+        {
+            //TODO
+            return 0;
+        }
+
+        public void storeImageId(DateTime date, int userId, int imageId)
+        {
+            //TODO
+        }
+
+        public void storeVideoId(DateTime date, int userId, int videoId)
+        {
+            //TODO
+        }
+
+        public void storeText(DateTime date, int userId, String text)
+        {
+            //TODO
+        }
+
+        public void createUserEntry(String username, String password)
+        {
+            //TODO
+        }
+
+        public bool isImpressionPresent(DateTime date, int userId)
+        {
+            //TODO
+            return false;
+        }
+    }
+}

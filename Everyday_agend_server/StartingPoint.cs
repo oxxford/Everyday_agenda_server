06da3ba1b74d2bdc9de5a50e5d4eed9238a49459
Dmitry Grigorev <gridmi007@gmail.com>
2018-04-26 01:12:20 +0300

    Try new video addition

--- Everyday_agend_server/StartingPoint.cs
+++ Everyday_agend_server/StartingPoint.cs
@@ -1,11 +1,9 @@
 ﻿using System;
 using System.IO;
-using System.Linq;
-using HttpMultipartParser;
 using Nancy;
 using Nancy.Hosting.Self;
 using Nancy.Responses;
-using Nancy.Security;
+
 
 namespace Everyday_agend_server
 {
@@ -28,7 +26,32 @@ namespace Everyday_agend_server
         {
             Get["/"] = parameters =>
             {
-                return Response.AsFile(@"C:\7\image112018.png","image/png");
+                //return Response.AsFile(@"C:\7\image112018.png","image/png");
+
+                //GenericFileResponse fileResponse = new GenericFileResponse(@"C:\7\image112018.png");
+                //return fileResponse;
+
+            
+                var response = new Response
+                {
+                    ContentType = "video/mp4",
+
+                    Contents = s =>
+                    {
+                        String fileName = @"C:\7\video112018.mp4";
+                        using (var stream = new FileStream(fileName, FileMode.Open))
+                            stream.CopyTo(s);
+                        s.Flush();
+                        s.Close();
+                    }
+                };
+
+                response.Headers.Add("Connection", "keep-alive");
+
+                long fileSize = new FileInfo(@"C:\7\video112018.mp4").Length;
+                response.Headers.Add("Content-length", fileSize.ToString());
+
+                return response;
             };
         }
     }
